714d9dd9b40db146b02265aa2c851b27f99b3656
Fernando Aparicio <faparicior@gmail.com>
2026-07-23 18:14:20 +0200

    feat: add @sessioflow/shared-domain and @sessioflow/shared-http error packages

---
+++ packages/modules/conference/src/domain/exceptions/domain-error.ts
@@ -0,0 +1,5 @@
+/**
+ * DomainError - Base class for all domain exceptions.
+ * Re-exported from @sessioflow/shared-domain/exceptions.
+ */
+export { DomainError } from '@sessioflow/shared-domain/exceptions';
--- packages/modules/conference/src/domain/exceptions/submission-date-in-past-error.ts
+++ packages/modules/conference/src/domain/exceptions/submission-date-in-past-error.ts
@@ -1,9 +1,11 @@
+import { DomainInvariantError } from '@sessioflow/shared-domain/exceptions';
+
 /**
  * SubmissionDateInPastError - Submission date is in the past.
  */
-export class SubmissionDateInPastError extends Error {
+export class SubmissionDateInPastError extends DomainInvariantError {
   constructor() {
-    super('Submission date must be in the future');
+    super('SUBMISSION_DATE_IN_PAST', 'Submission date must be in the future');
     this.name = 'SubmissionDateInPastError';
   }
 }
