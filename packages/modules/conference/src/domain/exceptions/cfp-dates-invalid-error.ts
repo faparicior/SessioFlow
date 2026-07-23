84c540398c727abebafebc4262f5d6dab1f5862e
Fernando Aparicio <faparicior@gmail.com>
2026-07-23 18:16:48 +0200

    feat: define default messages in domain exception constructors

--- packages/modules/conference/src/domain/exceptions/cfp-dates-invalid-error.ts
+++ packages/modules/conference/src/domain/exceptions/cfp-dates-invalid-error.ts
@@ -4,7 +4,7 @@ import { DomainInvariantError } from '@sessioflow/shared-domain/exceptions';
  * CfpDatesInvalidError - CfP dates are invalid (end date before start date).
  */
 export class CfpDatesInvalidError extends DomainInvariantError {
-  constructor(message: string) {
+  constructor(message = 'CfP end date must be after start date') {
     super('CFP_DATES_INVALID', message);
     this.name = 'CfpDatesInvalidError';
   }
